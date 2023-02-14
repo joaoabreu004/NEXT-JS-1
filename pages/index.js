f09ebb91eec9b2949cb864a08d1200d310a02944
João Abreu <jvabreusousa12@gmail.com>
2023-02-14 12:49:47 -0300

    Feat: FAQ Page's designer

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import HomeScreen from '../src/sreens/HomeScreen/index';
+// import Link from "next/link";
+// import React from "react";
+// import Footer from "../src/components/patterns/Footer";
+
+export default HomeScreen;
+
+
+
+// const Title = ({ children, as  }) => {
+//     const Tag = as
+//     return (
+//         <React.Fragment>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{
+//                 `
+//                     ${Tag}{
+//                         color: red;
+//                     }
+//                 `
+//             }
+//             </style>
+//         </React.Fragment>
+
+//     )
+// }
+
+
+
+
+// export default function HomePage() {
+//     return (
+//         <div>
+
+//             <Title as="h1">Alura Cases - Home</Title>
+//             <Link href="/faq">
+//                 Ir para o FAQ
+//             </Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
